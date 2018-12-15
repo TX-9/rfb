f4ec86008d387a5c0149c1714eb7379a60efe4e3
TX-9 <jacobleecs@gmail.com>
2018-12-15 16:38:57 -0500

    Initial application generated by JHipster-5.7.0

---
+++ src/main/java/com/jlab/web/rest/vm/ManagedUserVM.java
@@ -0,0 +1,19 @@
+package com.jlab.web.rest.vm;
+
+import com.jlab.service.dto.UserDTO;
+
+/**
+ * View Model extending the UserDTO, which is meant to be used in the user management UI.
+ */
+public class ManagedUserVM extends UserDTO {
+
+    public ManagedUserVM() {
+        // Empty constructor needed for Jackson.
+    }
+
+    @Override
+    public String toString() {
+        return "ManagedUserVM{" +
+            "} " + super.toString();
+    }
+}
