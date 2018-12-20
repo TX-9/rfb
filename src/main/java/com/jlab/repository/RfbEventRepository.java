ae550f4fd89a047aaff8292da71e1fcfc1026e20
TX-9 <jacobleecs@gmail.com>
2018-12-20 10:33:09 -0500

    add rfb event code service and test classes

--- src/main/java/com/jlab/repository/RfbEventRepository.java
+++ src/main/java/com/jlab/repository/RfbEventRepository.java
@@ -1,8 +1,11 @@
 package com.jlab.repository;
 
 import com.jlab.domain.RfbEvent;
+import com.jlab.domain.RfbLocation;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 
 
 /**
@@ -11,5 +14,6 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface RfbEventRepository extends JpaRepository<RfbEvent, Long> {
+    RfbEvent findByRfbLocationAndEventDate(RfbLocation location, LocalDate date);
 
 }
---
+++ src/test/java/com/jlab/repository/RfbEventRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.jlab.repository;
+
+import com.jlab.RfbApp;
+import com.jlab.bootstrap.RfbBootstrap;
+import com.jlab.domain.RfbEvent;
+import com.jlab.domain.RfbLocation;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {RfbApp.class})
+public class RfbEventRepositoryTest extends AbstractRepositoryTest {
+    @Before
+    public void setUp() throws Exception {
+        RfbBootstrap rfbBootstrap = new RfbBootstrap(rfbLocationRepository, rfbEventRepository,
+            rfbEventAttendanceRepository, rfbUserRepository);
+    }
+
+    @Test
+    public void findAllByRfbLocationAndEventDate() throws Exception {
+        RfbLocation aleAndTheWitch = rfbLocationRepository.findByLocationName("St Pete - Ale and the Witch");
+
+        assertNotNull(aleAndTheWitch);
+
+        RfbEvent event = rfbEventRepository.findByRfbLocationAndEventDate(aleAndTheWitch, LocalDate.now());
+
+        assertNotNull(event);
+
+    }
+}
